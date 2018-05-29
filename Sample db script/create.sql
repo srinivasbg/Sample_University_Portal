77225babb3d19ef59c5fde55c2dc743ccb605ac7
Srinivas Gudisagar <srinivasbg@gmail.com>
2018-05-29 11:35:10 -0700

    Adding sample scripts.

---
+++ Sample db script/create.sql
@@ -0,0 +1,171 @@
+﻿create database univ2;
+use univ2;
+
+/* Department table contains the existing department in the university and the faculty strength of each department */
+create table department
+(Deptname varchar(20) not null,
+facstrength int(2),
+primary key(Deptname));
+
+/*Student table has unique student id and other details of the student and
+ also says which department the student belongs to.The password cannot be empty
+ and checking of the length and being alphanumeric is done at the application level.
+ The student table is connected to "assosicate with relationship" as a many:1 association and since its redundant  the
+ relationship assisiocate with has been combined with the student table */
+
+create table student
+(studentid int(9),
+name varchar(25)not null,
+address varchar(50)not null,
+billaddress varchar(50),
+deptname varchar(20),
+email varchar(20),
+password varchar(20)not null,
+primary key(studentid),
+foreign key(deptname)references department(Deptname)
+);
+
+create table credit
+(studentid int(9),
+name varchar(25)not null,
+address varchar(50)not null,
+ccno varchar(20) unique,
+primary key(studentid,ccno),
+foreign key(studentid)references student(studentid)
+);
+
+/* Instructor table contains instructor information and the id for the instructor is unique
+and this field cannot be empty also deptname is stored in this table which is referenced in the department table.
+The instructor is connected to belongs to relation as a many:1 association and since its redundant  the relation
+belong to has been combined with the instructor table*/
+
+create table instructor
+(IID int(9)not null,
+name varchar(25)not null,
+homepage varchar(30),
+parttime bool,
+fulltime bool,
+email varchar(30),
+officeno varchar(25),
+password varchar(10),
+primary key(IID));
+
+Alter table instructor add column deptname varchar(20);
+Alter table instructor add foreign key(deptname) references department(Deptname);
+
+/* The course table is superset of all the courses offered in the university and its relavent information
+are stored in this table. The course name is unique and every course should have defined a noofunit */
+
+create table courses
+(cno int(5) not null,
+cname varchar(50),
+noofunits int(5)not null,
+Deptname varchar(20)not null,
+catalog varchar(20),
+primary key(cno,Deptname),
+foreign key(Deptname)references department(Deptname),
+unique(cname));
+
+
+/*This table has all the payment for the courses that student completed or are currently enrroled*/
+create table payment
+(receiptno int(10),
+amount int(10),
+ccno int(20),
+sid int(9) not null,
+cno int (5) not null,
+primary key(receiptno,sid,cno),
+foreign key (sid) references student(studentid),
+foreign key (cno) references courses(cno));
+
+/*contains information on student belonging to particular specilization and
+what core and elective courses are required for this specialization */
+create table specialization
+(sname varchar(20),
+corecourses varchar(20),
+electivecourses varchar(20),
+deptname varchar(20),
+stid int(9),
+primary key(sname,stid,deptname),
+foreign key (stid) references student(studentid),
+foreign key (deptname) references department(Deptname));
+Alter table specialization add column CoreCourse2 int(9);
+Alter table specialization add column ElectiveCourse2 int(9);
+
+/*Contains the information of the instructor who is taking the class and the students who are enrolled  for this class
+Here the waitlist no will be checked with the waitlist cap at the application level.Similiarly the section capacity will be
+checked at the application level*/
+
+create table section
+(secno int(5),
+seccap int(3),
+sem varchar(10),
+waitcap int(3),
+waitno int(3),
+cno int(5),
+deptname varchar(20),
+IID int(9),
+primary key(secno,cno,deptname,IID),
+foreign key(cno,deptname)references courses(cno,Deptname),
+foreign key(IID)references instructor(IID));
+ALTER TABLE `section` ADD `day` VARCHAR(10) NOT NULL AFTER `IID`;
+ALTER TABLE `section` ADD `time` TIME NOT NULL;
+ALTER TABLE `section` ADD `room` INT( 3 ) NOT NULL AFTER `Time`;
+ALTER TABLE `section` ADD UNIQUE (IID,day,time,room);
+
+
+
+
+create table enrollment
+(
+secno int(5),
+grade varchar(5),
+studentid int(9),
+rosterno int(5),
+gradeunits int(5),
+primary key(secno,studentid),
+foreign key (studentid) references student(studentid),
+foreign key (secno) references section(secno));
+ALTER TABLE `enrollment` ADD `waitno` INT( 3 ) NOT NULL AFTER `gradeunits`;
+ALTER TABLE `enrollment` CHANGE `grade` `grade` VARCHAR( 25 );
+
+
+/*this table provides  department chair details.The chair is also an instructor and hence derives the instrutor details
+from intructor table. The instructor id and the deptname referenced here cannot be modifies in thi relational table  */
+create table ischair
+(iid int(9),dname varchar(20),
+primary key(iid,dname),
+foreign key (iid) references instructor(IID) on update no action on delete no action,
+foreign key (dname) references department(Deptname) on update no action on delete no action);
+
+
+/*The teaches table is used store which instructor teaches which section.
+Here iid dname,studentid ,cno and secno cannot be modified. */
+create table teaches
+(iid int(9),
+secno int(5),
+primary key(iid,secno),
+foreign key (iid) references instructor(IID) on update no action on delete no action,
+foreign key (secno) references section(secno) on update no action on delete no action);
+
+
+/*This table maintains which student has taken what course studentid,dname,sname are not allowed to be modofied*/
+create table chooses
+(studentid int(9),sname varchar(20),
+primary key(studentid,sname),
+foreign key (studentid)references student(studentid)on update no action on delete no action,
+foreign key (sname)references specialization(sname) on update no action on delete no action);
+
+
+/* stores the prereq if any particular course it has.The prereq name can be got by the course table which is a
+superset of all courses */
+create table prereq(prereqid int(5),cno int(5),
+primary key(prereqid,cno),
+foreign key(cno) references courses(cno)on update no action on delete no action);
+
+
+/* stores the coreq if any particular course it has.The coreq name can be got by the course table which is a
+superset of all courses */
+create table coreq(coreqid int(5),cno int(5),
+primary key(coreqid,cno),
+foreign key(cno) references courses(cno)on update no action on delete no action);
---
+++ Sample db script/insert.sql
@@ -0,0 +1,143 @@
+﻿use univ2;
+
+INSERT INTO `univ2`.`department` (`Deptname`, `facstrength`) VALUES ('cs', '5'), ('cmpe', '5');
+INSERT INTO `univ2`.`department` (`Deptname`, `facstrength`) VALUES ('EE', '5');
+
+
+INSERT INTO `univ2`.`student` (`studentid`, `name`, `address`, `billaddress`, `deptname`, `email`, `password`)
+VALUES ('006376730', 'Jason', '65,pierce avenue, Market Street, San Jose CA 95112', '65,pierce avenue, Market Street, San Jose CA 95112', 'cs', 'jason@gmail.com', 'abc12345'),
+('006376731', 'Alex', '65,pierce avenue, Market Street, San Jose CA 95112', '65,pierce avenue, Market Street, San Jose CA 95112', 'cs', 'alex@yahoo.com', 'abc12345');
+
+
+INSERT INTO `univ2`.`student` (`studentid`, `name`, `address`, `billaddress`, `deptname`, `email`, `password`)
+VALUES ('006376732', 'John', '65,pierce avenue, Market Street, San Jose CA 95112', '65,pierce avenue, Market Street, San Jose CA 95112', 'cmpe', 'john@google.com', 'abc12345'),
+('006376733', 'Mark', '65,pierce avenue, Market Street, San Jose CA 95112', '65,pierce avenue, Market Street, San Jose CA 95112', 'cmpe', 'mark@intel.com', 'abc12345');
+
+
+INSERT INTO `univ2`.`student` (`studentid`, `name`, `address`, `billaddress`, `deptname`, `email`, `password`)
+VALUES ('006376734', 'Amy', '65,pierce avenue, Market Street, San Jose CA 95112', '65,pierce avenue, Market Street, San Jose CA 95112', 'EE', 'amy@hollywood.com', 'abc12345'),
+('006376735', 'shauna', '65,pierce avenue, Market Street, San Jose CA 95112', '65,pierce avenue, Market Street, San Jose CA 95112', 'EE', 'shauna@hotmail.com', 'abc12345'),('006376737', 'Brad', '65,pierce avenue, Market Street, San Jose CA 95112', '65,pierce avenue, Market Street, San Jose CA 95112', 'cs', 'brad@google.com', 'abc12345');
+
+insert into student(studentid,name,address,billaddress,deptname,email,password)values(6376736,'Anjelina','65,pierce avenue, Market Street, San Jose CA 95112','65,pierce avenue, Market Street, San Jose CA 95112','cs','anjelina@hotmail.com','abc12345');
+
+
+
+
+
+INSERT INTO `univ2`.`instructor` (`IID`, `name`, `homepage`, `parttime`, `fulltime`, `email`, `officeno`, `password`, `deptname`) VALUES ('11', 'Magdalini Erinaki', 'http://www.facebook.com', '0', '1', 'magda@yahoo.com', '101', 'abc12345', 'cs'), ('22', 'Dan Harkey', 'http://www.yahoo.com', '0', '1', 'dan@yahoo.com', '102', 'abc12345', 'cs'), ('33', 'Donald Hung', 'http://www.yahoo.com', '0', '1', 'donald@yahoo.com', '103', 'abc12345', 'cs'), ('44', 'Xiao su', 'http://www.yahoo.com', '0', '1', 'xiao@yahoo.com', '104', 'abc12345', 'cs'), ('55', 'richard sinn', 'http://www.yahoo.com', '0', '1', 'richard@yahoo.com', '105', 'abc12345', 'cs'),('66', 'Rod fatoohi', 'http://www.yahoo.com', '0', '1', 'xiao@yahoo.com', '106', 'abc12345', 'cmpe');
+
+
+
+INSERT INTO `univ2`.`courses` (`cno`, `cname`, `noofunits`, `Deptname`, `catalog`)
+VALUES ('226', 'Database Systems', '4', 'cs', 'MySQL queries'), ('227', 'Web Mining', '4', 'cs', 'Data Warehousing');
+
+
+INSERT INTO `univ2`.`courses` (`cno`, `cname`, `noofunits`, `Deptname`, `catalog`)
+VALUES ('228', 'Business Intelligence', '4', 'cs', 'Business Intelligenc'), ('229', 'Advanced Database', '4', 'cs', 'Database Progams');
+
+
+INSERT INTO `univ2`.`courses` (`cno`, `cname`, `noofunits`, `Deptname`, `catalog`)
+VALUES ('206', 'Computer Networks', '4', 'cs', 'OSI model'), ('207', 'Network Programming', '4', 'cs', 'Distributed System');
+
+INSERT INTO `univ2`.`courses` (`cno`, `cname`, `noofunits`, `Deptname`, `catalog`)
+VALUES ('208', 'Network Architecture', '4', 'cs', 'Protocols study'), ('209', 'Network Security', '4', 'cs', 'Security Concepts');
+
+INSERT INTO `univ2`.`section` (`secno`, `seccap`, `sem`, `waitcap`, `waitno`, `cno`, `deptname`, `IID`,`day`,`time`,`room`)
+VALUES ('99', '3', 'f09', '3', '0', '226', 'cs', '11','monday','18:00:00','333'), ('100', '3', 's09', '3', '0', '226', 'cs', '11','tuesday','18:00:00','335'), ('101', '3', 's10', '3', '0', '227', 'cs', '22','monday','18:00:00','337'), ('102', '2', 'f09', '0', '0', '227', 'cs', '44','friday','18:00:00','339'), ('103', '3', 's10', '3', '0', '228', 'cs', '33','monday','18:00:00','400');
+
+INSERT INTO `univ2`.`section` (`secno`, `seccap`, `sem`, `waitcap`, `waitno`, `cno`, `deptname`, `IID`,`day`,`time`,`room`)
+VALUES ('104', '3', 's10', '3', '0', '229', 'cs', '44','monday','18:00:00','343'), ('105', '3', 's10', '3', '0', '229', 'cs', '44','tuesday','18:00:00','353');
+
+
+INSERT INTO `univ2`.`section` (`secno`, `seccap`, `sem`, `waitcap`, `waitno`, `cno`, `deptname`, `IID`,`day`,`time`,`room`)
+VALUES ('106', '3', 's10', '3', '0', '206', 'cs', '11','wednesday','18:00:00','333'), ('107', '3', 's10', '3', '0', '206', 'cs', '11','thursday','18:00:00','333'), ('108', '3', 's10', '3', '0', '206', 'cs', '11','friday','18:00:00','333'), ('109', '3', 'f09', '3', '0', '207', 'cs', '22','wednesday','18:00:00','363'), ('110', '3', 'f09', '3', '0', '207', 'cs', '22','thursday','18:00:00','373');
+
+INSERT INTO `univ2`.`section` (`secno`, `seccap`, `sem`, `waitcap`, `waitno`, `cno`, `deptname`, `IID`,`day`,`time`,`room`)
+VALUES ('111', '3', 's09', '3', '0', '208', 'cs', '33','wednesday','18:00:00','433'), ('112', '3', 's09', '3', '0', '208', 'cs', '33','tuesday','18:00:00','473');
+
+INSERT INTO `univ2`.`section` (`secno`, `seccap`, `sem`, `waitcap`, `waitno`, `cno`, `deptname`, `IID`,`day`,`time`,`room`)
+VALUES ('113', '3', 's09', '3', '0', '209', 'cs', '44','wednesday','18:00:00','393'), ('114', '3', 's09', '3', '0', '209', 'cs', '44','thursday','18:00:00','499');
+
+
+
+
+insert into teaches(iid,secno)values('11','99'),('11','100'),('22','101'),('44','102'),('33','103'),('44','104'),('44','105'),('11','106'),('11','107'),('11','108'),('22','109'),('22','110'),('33','111'),('33','112'),('44','113'),('44','114');
+insert into ischair(iid,dname)values('22','cs'),('66','cmpe');
+
+
+INSERT INTO `univ2`.`courses` (`cno`, `cname`, `noofunits`, `Deptname`, `catalog`)
+VALUES ('272', 'Enterprise Overview', '4', 'cmpe', 'Ent concepts'), ('273', 'Enterprise systems', '4', 'cmpe', 'Java pgms');
+
+INSERT INTO `univ2`.`courses` (`cno`, `cname`, `noofunits`, `Deptname`, `catalog`)
+VALUES ('274', 'Service Oriented Arch', '4', 'cmpe', 'SOA concepts'), ('275', 'XML', '4', 'cmpe', 'xml');
+
+
+INSERT INTO `univ2`.`courses` (`cno`, `cname`, `noofunits`, `Deptname`, `catalog`)
+VALUES ('202', 'System Software', '4', 'cmpe', 'Modelling'), ('203', 'Engr Management', '4', 'cmpe', 'Management Concepts');
+
+INSERT INTO `univ2`.`courses` (`cno`, `cname`, `noofunits`, `Deptname`, `catalog`)
+VALUES ('204', 'Operating Systems', '4', 'cmpe', 'Os concepts'), ('205', 'Distributed Systems', '4', 'cmpe', 'Distributed Systems');
+
+
+
+INSERT INTO `univ2`.`courses` (`cno`, `cname`, `noofunits`, `Deptname`, `catalog`)
+VALUES ('220', 'Logic Design', '4', 'EE', 'gates design'), ('221', 'Electical Circuits', '4', 'EE', 'EC analysis');
+
+INSERT INTO `univ2`.`courses` (`cno`, `cname`, `noofunits`, `Deptname`, `catalog`)
+VALUES ('222', 'Digital Circuits', '4', 'EE', 'AC/DC'), ('223', 'Analog Circuits', '4', 'EE', 'Analog Circuits');
+
+
+INSERT INTO `univ2`.`courses` (`cno`, `cname`, `noofunits`, `Deptname`, `catalog`)
+VALUES ('225', 'Cryptography', '4', 'EE', 'Cryptography concept'), ('230', 'Voip', '4', 'EE', 'Voip Concepts');
+
+INSERT INTO `univ2`.`courses` (`cno`, `cname`, `noofunits`, `Deptname`, `catalog`)
+VALUES ('231', 'Electrical Security', '4', 'EE', 'Electrical Security'), ('232', 'Internetworking', '4', 'EE', 'Fiber Optics');
+
+
+
+INSERT INTO `univ2`.`specialization` (`sname`, `corecourses`, `electivecourses`, `deptname`, `stid`, `CoreCourse2`, `ElectiveCourse2`)
+VALUES ('Database', '226', '228', 'cs', '006376730', '227', '229'), ('Networking', '206', '208', 'cs', '006376731', '207', '209');
+
+
+INSERT INTO `univ2`.`specialization` (`sname`, `corecourses`, `electivecourses`, `deptname`, `stid`, `CoreCourse2`, `ElectiveCourse2`)
+VALUES ('Enterprise', '272', '274', 'cmpe', '006376732', '273', '275'), ('System Software', '202', '204', 'cmpe', '006376733', '203', '205');
+
+INSERT INTO `univ2`.`specialization` (`sname`, `corecourses`, `electivecourses`, `deptname`, `stid`, `CoreCourse2`, `ElectiveCourse2`)
+VALUES ('VLSI', '220', '222', 'EE', '006376734', '221', '223'), ('Enetworking', '225', '231', 'EE', '006376735', '230', '232');
+
+insert into specialization(sname,corecourses,electivecourses,deptname,stid,CoreCourse2,ElectiveCourse2)values('Networking',206,208,'cs',6376736,207,209);
+insert into specialization(sname,corecourses,electivecourses,deptname,stid,CoreCourse2,ElectiveCourse2)values('Networking',206,208,'cs',6376737,207,209);
+
+
+
+
+
+INSERT INTO `univ2`.`enrollment` (`secno`, `grade`, `studentid`, `rosterno`, `gradeunits`, `waitno`)
+VALUES ('100', 'A', '006376730', '2', '4', '0'), ('102', 'In Progress', '006376734', '1', '0', '0'),('102', 'In Progress', '006376735', '2', '0', '0'),('102', 'In Progress', '006376733', '3', '0', '0');
+
+INSERT INTO `univ2`.`enrollment` (`secno`, `grade`, `studentid`, `rosterno`, `gradeunits`, `waitno`)
+VALUES ('105', 'In Progress', '006376737', '0', '0', '1');
+
+insert into enrollment(secno,grade,studentid,rosterno,gradeunits,waitno)values(113,'B',6376737,1,3,0);
+insert into enrollment(secno,grade,studentid,rosterno,gradeunits,waitno)values(114,'B',6376731,1,3,0);
+insert into enrollment(secno,grade,studentid,rosterno,gradeunits,waitno)values(104,'In Progress',6376731,1,0,0);
+
+insert into payment values (1000,1000,123456789,6376730,226);
+insert into payment values (1001,1000,123456789,6376730,228);
+insert into payment values (1000,1005,123456789,6376730,227);
+
+
+insert into credit values(6376730,'Jason','65 market street san jose',123456789);
+insert into credit values(6376730,'JASON','65 market street san jose',123456798);
+
+INSERT INTO `univ2`.`chooses` (`studentid`, `sname`) VALUES ('006376730', 'Database'), ('006376731', 'Networking'), ('006376732', 'Enterprise'), ('006376733', 'System Software'), ('006376734', 'VLSI');
+INSERT INTO `univ2`.`chooses` (`studentid`, `sname`) VALUES ('006376735', 'Enetworking'), ('006376736', 'Networking');
+INSERT INTO `univ2`.`chooses` (`studentid`, `sname`) VALUES ('006376737', 'Networking');
+
+
+
+
+
+
+
+INSERT INTO `univ2`.`prereq` (`prereqid`, `cno`) VALUES ('226', '227'), ('228', '229');
